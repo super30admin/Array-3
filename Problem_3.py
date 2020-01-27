e3ead2145256add100c2bb303970203c25bb8089
Rishika Machina <39758648+RishikaMachina@users.noreply.github.com>
2020-01-27 11:01:37 -0800

    Update Problem_3.py

--- Problem_3.py
+++ Problem_3.py
@@ -10,3 +10,15 @@ class Solution:
         for i in range(k):
             temp = nums.pop()
             nums.insert(0,temp)
+
+# Runs on Leetcode - Optimized
+# Runtime is O(n) and space is O(1)
+            
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        size = len(nums)
+        if k > size:
+            k = k % size 
+        nums[:] = nums[:][::-1]
+        nums[:k] = nums[:k][::-1]
+        nums[k:] = nums[k:][::-1]
