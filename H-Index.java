374f38bd56e6ed8c527117d0ac3a941cee6750ff
Sachin Sharma <sachinshama3191@gmail.com>
2020-03-01 18:47:26 -0800

    Add files via upload

---
+++ H-Index.java
@@ -0,0 +1,22 @@
+//Time Complexity : O(N) for Linear SEarch Approach 
+//Space Complexity : O(1)
+//Did this code successfully run on Leetcode : Yes
+//Any problem you faced while coding this : No
+
+//Your code here along with comments explaining your approach
+
+/*
+*/
+
+public class Solution {
+    public int hIndex(int[] citations) {
+        // sorting the citations in ascending order
+        Arrays.sort(citations);
+        int idx = 0, n = citations.length;
+    for(int c : citations) {
+      if (c >= n - idx) return n - idx;
+      else idx++;
+    }
+    return 0;
+    }
+}
