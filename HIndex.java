598c2629323490fa7893d32ee0bf0583e6cfe488
SomiDeev <sowmyadvn.92@gmail.com>
2020-04-06 20:14:06 -0700

    Updated after class

--- HIndex.java
+++ HIndex.java
@@ -24,3 +24,39 @@ class Solution {
         return n - low;
     }
 }
+
+/*
+ ============================ Counting sort ===========================
+ Time: O(N) space: O(N)
+ Approach:
+ ========
+ 1. Calculate citations count and store in array
+ 2. Store running sum as we move from right to left and update it
+ 3. Stop whenever index is less than or equal to runningsum
+*/
+
+class Solution {
+    public int hIndex(int[] citations) {
+       if(citations == null || citations.length == 0) return 0; 
+        int n = citations.length;
+        int[] buckets = new int[n+1];
+        
+        //calculate counts of citations and store in buckets array
+        for(int c: citations) {
+            if(c > n) 
+                buckets[n]++;
+            else
+                buckets[c]++;
+        }
+        
+        int runningSum = 0, index = n; 
+        // calculate running sum and check if index less than or equal to running sum at every index
+        while(index >= 0) {
+            runningSum += buckets[index];
+            if(index <= runningSum)
+                return index;
+            index--;
+        }
+        return -1;
+    }
+}
--- TrappingRainWater.java
+++ TrappingRainWater.java
@@ -1,4 +1,69 @@
 /*
+ ====================BRUTEFORCE =========================
+
+Time: O(N)
+Space: O(1)
+Approach:
+========
+1. Use 2-pointer technique to keep track of left and right pointers.
+2. Keep track of maxLeft and maxRight by comparing with current left and right values respectively.
+3. Uses basic idea that of left and right, whichever is small would be the max size of container that can
+trap rain water.  
+*/
+class Solution {
+    public int trap(int[] height) {
+        int leftMax = 0, rightMax = 0; 
+        int totalWater = 0, n = height.length; 
+        // For every index, get left max and right max on the fly and calculate trapped water
+        for(int i = 0; i < n; i++) {
+            leftMax = 0; 
+            rightMax = 0; 
+            for(int j = i; j >= 0; j--)
+                leftMax = Math.max(leftMax, height[j]); 
+            for(int j = i; j < n; j++)
+                rightMax = Math.max(rightMax, height[j]); 
+            totalWater += Math.min(leftMax, rightMax) - height[i];
+        }
+        return totalWater;
+    }
+}
+
+/*
+======================= Approach-2 ============================
+Time:O(N) space: O(N)
+Approach:
+=========
+1. Use left and right arrays to store leftmax and rightmax at every index
+2. Now, use these array values at every index to calculate trapped rain water
+*/
+class Solution {
+    public int trap(int[] height) {
+        if(height == null || height.length == 0) return 0; 
+        int n = height.length; 
+        int[] left = new int[n];
+        int[] right = new int[n]; 
+        int leftMax = 0, rightMax = 0, totalWater = 0; 
+        
+        //calculate left max for each index
+        for(int i = 0; i < n; i++) {
+            leftMax = Math.max(leftMax, height[i]); 
+            left[i] = leftMax; 
+        }
+        
+        //calculate right max for each index
+        for(int i = n-1; i >= 0; i--) {
+            rightMax = Math.max(rightMax, height[i]); 
+            right[i] = rightMax; 
+        }
+        
+        // calculate total trapped water 
+        for(int i = 0; i < n; i++)
+            totalWater += Math.max(0, Math.min(left[i], right[i]) - height[i]); 
+        
+        return totalWater;
+    }
+}
+/*
 Time: O(N)
 Space: O(1)
 Approach:
