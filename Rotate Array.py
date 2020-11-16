a6c4a1a6312aa79423fe280a3596403ed0266ef0
Piyush Kurkure <piyushk10@gmail.com>
2020-11-16 09:20:13 -0800

    Tried Arrays-3

---
+++ Rotate Array.py
@@ -0,0 +1,17 @@
+# TC: O(N^2)
+# SC: O(1)
+# Yes, It ran on Leetcode
+# No problems
+
+class Solution:
+    def rotate(self, nums, k) -> None:
+        k %= len(nums)
+        self.reverse(nums,0,len(nums)-1)
+        self.reverse(nums,0,k-1)
+        self.reverse(nums,k,len(nums)-1)
+        
+    def reverse(self,nums,l,r):
+        while l<r:
+            nums[l],nums[r] = nums[r],nums[l]
+            l += 1
+            r -= 1
