75c2fb45c7432b2e05337e9a7a73807e136243ec
Vijay Raja <jobprepnyu@gmail.com>
2020-12-27 21:29:25 -0500

    Update rotate.py

--- rotate.py
+++ rotate.py
@@ -31,3 +31,18 @@ class Solution:
 
 Time: O(N)
 Space: O(1)
+
+Method : 2
+    
+class Solution:
+def rotate(self, nums: List[int], k: int) -> None:
+    k%=len(nums)
+    def reverse(i, j):
+        while i<j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i+=1
+            j-=1
+
+    reverse(0, len(nums)-1)
+    reverse(0, k-1)
+    reverse(k, len(nums)-1)
