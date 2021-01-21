78ff1355bffc3b6f0c4efdc7850a3d98256a1285
Rishi Raj <rishiraj824@gmail.com>
2021-01-21 01:17:11 -0500

    Create h-index.java

---
+++ h-index.java
@@ -0,0 +1,16 @@
+// O(NlgN)
+// O(1)
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        Arrays.sort(citations);
+        for(int i=0;i<n;i++) {
+            int diff = n - i;
+            if(citations[i] >= diff) {
+                return diff;
+            }
+        }
+        
+        return 0;
+    }
+}
