71e4b47671cb14b7482d10f5123f295beeebc66b
Rishi Raj <rishiraj824@gmail.com>
2021-01-22 07:11:29 -0500

    Update h-index.java

--- h-index.java
+++ h-index.java
@@ -1,3 +1,33 @@
+// O(N), O(N)
+class Solution {
+    public int hIndex(int[] citations) {
+        if(citations == null || citations.length == 0) {
+            return 0;
+        }
+        int n = citations.length;
+        
+        int[] counts = new int[n+1];
+        
+        for(int i=0;i<n;i++) {
+            if(citations[i]>=n) {
+                counts[n]++;
+            }
+            else {
+                counts[citations[i]]++;
+            }
+        }
+        int sum = 0;
+        for(int i=n;i>=0;i--) {
+            sum += counts[i];
+            
+            if(sum>=i) {
+                return i;
+            }
+        }
+        
+        return 0;
+    }
+}
 // O(NlgN)
 // O(1)
 class Solution {
