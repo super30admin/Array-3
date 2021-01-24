4747294b28ff1a1a532eddb5326036b19d315346
Giridhar <4182759+girisub@users.noreply.github.com>
2021-01-24 02:43:47 -0500

    Add files via upload

---
+++ rotate-array.cpp
@@ -0,0 +1,11 @@
+//Time - O(n)
+//Space - O(1)
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k%nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+    }
+};
