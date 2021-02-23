47e978c35411d8711a0abf2918d9dea733d83d8f
Nilesh Deokar <76528900+nilesh-525@users.noreply.github.com>
2021-02-23 18:21:13 -0500

    Update Hindex2.java
    added optimal approach

--- Hindex2.java
+++ Hindex2.java
@@ -22,4 +22,43 @@ class Hindex2 {
     
         return 0;
     }
+  
+  
+   /*
+      Time : O(N)  
+      Space : O(N)
+      Leetcode : YES
+   */
+  
+  /*
+    Approach : Bucket sort
+    1. we don't care how many citations author has beyond given N,
+    2. Count all of citations >= n in one bucket.
+    3. iterative from back on buckets array and as sum >= i return i
+  */
+   public int hIndex(int[] citations) {
+        if(citations == null) return 0;
+        int n = citations.length;
+        
+        int[] buckets = new int[n + 1];
+        
+        for(int i = 0; i < n; i++){
+            if(citations[i] >= n){
+                buckets[n]++;
+            }else{
+               buckets[citations[i]]++; 
+            }
+            
+        }
+        
+        int sum = 0;
+        for(int i = n; i>=0 ; i--){
+            sum += buckets[i];
+            if(sum >= i){
+                return i;
+            }
+        }
+    
+        return 0;
+    }
 } 
