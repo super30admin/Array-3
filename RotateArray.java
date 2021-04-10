79122fdb833e69c4cb100e99611989f0b583f08f
Gurpreet Singh <gsn10397@gmail.com>
2021-04-10 14:58:20 -0400

    Completed Array-3

---
+++ RotateArray.java
@@ -0,0 +1,35 @@
+package Array-3;
+// 
+public class RotateArray {
+    class Solution {
+
+        public void rotate(int[] nums, int k) {
+           int n = nums.length;
+           if(n==0 || n == 1) return;
+           if(k>=n) k = k%n;
+           for(int i=0;i<n/2;i++)
+           {
+               int temp = nums[i];
+               nums[i] = nums[n-i-1];
+               nums[n-i-1] = temp;
+           }
+             
+           for(int i=0;i<k/2;i++)
+           {
+               int temp = nums[i];
+               nums[i] = nums[k-i-1];
+               nums[k-i-1] = temp;
+           }
+         
+           for(int i=0;i<(n-k)/2;i++)
+           {
+               int temp = nums[i+k];
+               nums[i+k] = nums[n-i-1];
+               nums[n-i-1] = temp;
+           } 
+             
+            
+        }
+    }
+    
+}
