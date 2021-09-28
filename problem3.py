1af59626cda8a9289f6e1e189fa20ec864d21c2c
Yash Naredi <yashnaredi26@gmail.com>
2021-09-28 13:06:01 -0400

    completed Array-3

---
+++ problem3.py
@@ -0,0 +1,22 @@
+#rotate array
+#TC:O(n) wher n is the nums
+#SC:O(1)
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k %= n
+        
+        start = count = 0
+        while count < n:
+            current, prev = start, nums[start]
+            while True:
+                next_idx = (current + k) % n
+                nums[next_idx], prev = prev, nums[next_idx]
+                current = next_idx
+                count += 1
+                
+                if start == current:
+                    break
+            start += 1
