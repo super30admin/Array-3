9503663a50f5b22fbbdfd5c813505ab798737800
sbagul077 <sbagul077@gmail.com>
2021-10-20 21:14:16 -0400

    Update 118. Trapping Rain Water.py

--- 118. Trapping Rain Water.py
+++ 118. Trapping Rain Water.py
@@ -2,19 +2,21 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         if len(height) == 0 or height is None:
             return
-        lw, rw = 0, 0
-        l, r = 0, len(height) - 1
+        lw, rw = 0, 0  #height
+        l, r = 0, len(height) - 1 # pointer. Left and right
         result = 0
 
-        while l < r:
+        while l <= r:
             if lw <= rw:
+                #processing from left side
                 if lw > height[l]:
                     result += lw - height[l]
                 else:
                     lw = height[l]
                 l += 1
             else:
-                if rw > height[r]
+                # processing from right side
+                if rw > height[r]:
                     result += rw - height[r]
                 else:
                     rw = height[r]
@@ -26,5 +28,7 @@ class Solution:
 # Space complexity : O(n)
 # Did this code successfully run on Leetcode : yes
 # Any problem you faced while coding this : Syntax errors and accidentally assigned l and r pointer as heights and lw and rw as index(pointers)
+
+
 
 
