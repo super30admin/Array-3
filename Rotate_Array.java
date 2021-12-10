93e22566c2aff27e60c9cdbfed3deb1baa27427a
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2021-12-09 21:47:29 -0500

    Create Rotate_Array.java

---
+++ Rotate_Array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if(nums == null || nums.length < 2) return;
+        
+        int n = nums.length - 1;
+        
+        if(k > nums.length){
+            k = k % nums.length;
+        }
+        reverse(nums, 0, n);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n);
+    }
+    
+    private void reverse(int [] nums, int l, int r){
+        while(l < r){
+            swap(nums, l, r);
+            l++; r--;
+        }
+    }
+    
+    private void swap(int [] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+
+//TC: O(N)
+//SC: O(1)
