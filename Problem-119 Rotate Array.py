1397f08573282ce6fce775eab3a6a888f825871c
Adhish Shrivastava <adhish70@gmail.com>
2021-12-17 09:35:06 -0500

    Problem 119 done

--- Problem-119 Rotate Array.py
+++ Problem-119 Rotate Array.py
@@ -1,11 +1,26 @@
 # 189. Rotate Array
 # https://leetcode.com/problems/rotate-array/
 
-# Logic: 
+# Logic: Reverse the entire array. Then reverse 0 to k-1 
+# elements and then reverse k to n-1 elements.
 
-# Time Complexity: 
-# Space Complexity: 
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 
 class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        pass
+    def _reverse(self, nums, i, j):
+        while i < j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j -= 1
+        
+    def rotate(self, nums, k) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = (k % n)
+        
+        self._reverse(nums, 0, n-1)
+        self._reverse(nums, 0, k-1)
+        self._reverse(nums, k, n-1)
