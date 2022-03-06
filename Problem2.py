364a290fa9e0f482ac22effd726e1d953c3063bf
shrivatsav <kumarshrivatsav98@gmail.com>
2022-03-05 21:05:27 -0600

    Add Solutions

---
+++ Problem2.py
@@ -0,0 +1,13 @@
+# // Time Complexity : O(nlogn) where n is the length of the citations list.  
+# // Space Complexity : O(1).
+# // Did this code successfully run on Leetcode : Yes
+# // Any problem you faced while coding this : Class Solution.
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        r = len(citations)
+        #find first idx where <= the num
+        for i, n in enumerate(citations):
+            if r-i <= n:
+                return r-i
+        return 0
