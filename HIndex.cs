75fa9a324107b0c57bcce010e697d687a91fa95e
nilesh-ghodke <99153455+nilesh-ghodke@users.noreply.github.com>
2022-06-09 15:09:04 -0700

    Add files via upload

---
+++ HIndex.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Array
+{
+    class HIndex
+    {
+        /*
+         * T.C: O(n) where n is no of citations
+         * S.c: O(n) buckets array contains the number of papers
+         */
+        public int findHIndex(int[] citations)
+        {
+            if (citations == null || citations.Length == 0)
+                return 0;
+
+            int n = citations.Length;
+            int[] buckets = new int[n + 1];
+
+            for (int i = 0; i < citations.Length; i++)
+            {
+                if (citations[i] >= n)
+                {
+                    buckets[n]++;
+                }
+                else
+                {
+                    buckets[citations[i]]++;
+                }
+            }
+
+            int sum = 0;
+
+            for (int i = n; i >= 0; i--)
+            {
+                sum += buckets[i];
+
+                if (sum >= i)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
