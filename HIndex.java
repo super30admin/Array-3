1b18485352b5b621dda9fb651ce919144b5682bc
Akshay Thorve <72192352+ds-algo-akshay@users.noreply.github.com>
2022-07-06 14:18:55 -0700

    Update HIndex.java

--- HIndex.java
+++ HIndex.java
@@ -1,17 +1,33 @@
 class Solution {
-    /**
-     * Sort array find breach return diff.
-     * TC: O(nlogn)
-     * sc: O(1)
-     */
-
-    public int hIndex(int[] citations) {
-        Arrays.sort(citations);
-        for (int i = 0; i < citations.length; i++) {
-            if (citations[i] >= citations.length - i) {
-                return citations.length - i;
-            }
+    public void nextPermutation(int[] nums) {
+        if (nums.length == 0) {
+            return;
         }
-        return 0;
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1, nums.length - 1);
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
     }
 }
