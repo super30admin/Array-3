1cd189c69a026aa9c48fe94df04a3c35b8a487f4
sivakami-projects <126191544+sivakami-projects@users.noreply.github.com>
2023-05-07 17:40:19 -0700

    Create RotateArray.java

---
+++ RotateArray.java
@@ -0,0 +1,39 @@
+/*
+Time complexity -  O(n)
+Space complexity - O(1)
+*/
+class Solution {
+    public void rotate(int[] nums, int k) {
+        
+        if(nums == null || nums.length == 0 || k <= 0)
+            return;
+        
+        int len = nums.length;
+        k = k > len ? k % len : k;
+        int firstHalfLen = len - k;
+        
+        int left = 0, right = firstHalfLen -1;
+        while(left < right) {
+            int temp = nums[left];
+            nums[left++] = nums[right];
+            nums[right--] = temp;
+        }
+        
+        left = 0;
+        right = nums.length - 1;
+        while(left < right) {
+            int temp = nums[left];
+            nums[left++] = nums[right];
+            nums[right--] = temp;
+        }
+        
+        left = 0;
+        right = k - 1;
+        while(left < right) {
+            int temp = nums[left];
+            nums[left++] = nums[right];
+            nums[right--] = temp;
+        }
+        return;
+    }
+}
