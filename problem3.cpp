74fc28f7fbb3709523b3f2a38e6d0781dc1a904b
thanujkumarj <thanujkumarj@gmail.com>
2023-06-22 08:25:22 -0700

    Rotate Array

---
+++ problem3.cpp
@@ -0,0 +1,21 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+// we reverse the given array and then 
+// we reverse firt k elements, then we reverse remaining elements
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k%n;
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+    }
+};
