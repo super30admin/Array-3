bcaaef04d1402e2500aa1868ac038902f72bad54
Sana Pathan <sanapathan28@gmail.com>
2023-08-02 23:54:12 -0700

    https://leetcode.com/problems/h-index/ https://leetcode.com/problems/trapping-rain-water/ https://leetcode.com/problems/rotate-array

---
+++ Problem274.java
@@ -0,0 +1,72 @@
+import java.util.Arrays;
+
+public class Problem274 {
+    //TC=O(n)
+//SC=O(1)
+    public int trap1(int[] height) {
+        int n=height.length;
+        int max=0;
+        int maxHeightIdx=-1;
+        int maxArea=0;
+
+        for(int i=0;i<n;i++){
+            if(height[i]>max){
+                max=height[i];
+                maxHeightIdx=i;
+            }
+        }
+        int lw=0;
+        for(int j=0;j<maxHeightIdx;j++){
+            if(height[j]<lw){
+                maxArea+=lw-height[j];
+            }
+            else{
+                lw=height[j];
+            }
+        }
+        int rw=0;
+        for(int j=n-1;j>maxHeightIdx;j--){
+            if(height[j]<rw){
+                maxArea+=rw-height[j];
+            }
+            else{
+                rw=height[j];
+            }
+        }
+        return maxArea;
+    }
+    public int trap(int[] height) {
+        int rightPointer=height.length-1;
+        int lw=0;
+        int rw=0;
+        int leftPointer=0;
+        int maxArea=0;
+        while(leftPointer<=rightPointer){
+            if(lw<=rw){
+                if(height[leftPointer]<lw){
+                    maxArea+=lw-height[leftPointer];
+                }
+                else{
+                    lw=height[leftPointer];
+                }
+                leftPointer++;
+            }
+            else{
+                if(height[rightPointer]<rw){
+                    maxArea+=rw-height[rightPointer];
+                }
+                else{
+                    rw=height[rightPointer];
+                }
+                rightPointer--;
+            }
+        }
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        Problem274 problem=new Problem274();
+        System.out.println(problem.trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
+        System.out.println(problem.trap1(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
+    }
+}
