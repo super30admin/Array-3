3efec89e45a4f1c97912a07d211625a56ade0d80
Khushbu-Kareliya <154071841+Khushbu-Kareliya@users.noreply.github.com>
2024-01-12 14:29:06 -0600

    Update 274_HIndex.java - added Binary search approach

--- 274_HIndex.java
+++ 274_HIndex.java
@@ -1,3 +1,6 @@
+//H-Index and H-Index-II added
+
+
 /*
 
 citations is not in sorted manner
@@ -60,6 +63,46 @@ class Solution {
                 return i;
         }
         // never found it
+        return 0;
+    }
+}
+
+/*
+Binary Search approach :
+ 
+1. Take low and high pointer on bounds for binary search 
+2. we'll look for condition where mid's value >= diff. 
+2a. if we find mid; we have to make sure, that's our left most mid, where condition satisifed.
+2b. So to check that, we check the adjacent element, and diff at that point. 
+note -> the diff calculated is for mid position only. For mid-1; ths diff will be diff+1.
+3.if we found mid-1 satisfying condition, we update high = mid-1; else return the mid
+4. in case, where mid's val < difff, perform low = mid+1;
+
+TC: O(log n)
+SC: O(1)
+*/
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        
+        int[] arr = new int[n+1];
+        int low =0, high =n-1;
+        
+        while(low <= high)        {
+            int mid = low + (high-low)/2;
+            int diff = n-mid;
+            if(citations[mid] >= diff) {
+                //if found match on above conditon, let's explore where this condition is match on the left most side. 
+              if(mid>0 && citations[mid-1] >= diff+1) {
+                    high = mid -1;
+                }
+                else
+                    return diff;
+            }
+            else
+                low = mid+1; //diff is greater to mid's value, go on right side.
+        }
+        //never found it
         return 0;
     }
 }
