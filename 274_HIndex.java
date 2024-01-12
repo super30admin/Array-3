8d99eca7609211fc255bec7592d934453a6d49f7
Khushbu-Kareliya <154071841+Khushbu-Kareliya@users.noreply.github.com>
2024-01-11 20:54:28 -0600

    H-Index II both approaches added.

--- 274_HIndex.java
+++ 274_HIndex.java
@@ -3,7 +3,7 @@
 citations is not in sorted manner
 if sorted, we can just us n-h approach as folows
 1. Sort the array. 
-2. h index will lie from 1 to n = length of the array 
+2. h index will lie from 0 to n = length of the array 
 3. find the index, where, h index value <= citations values. 
 4. return that index. 
 
@@ -20,6 +20,44 @@ class Solution {
             int hindex = n - i;
             if (citations[i] >= hindex)
                 return hindex;
+        }
+        // never found it
+        return 0;
+    }
+}
+
+/*
+ * approach
+ * 1. take the new array of size n+1
+ * 2. go to the citations, and mark that indx in arr[] an increase it by 1.
+ * 3. when find a value bigger than the n, just keep incrementing the last
+ * index.
+ * 4. run a nother for loop to find the H index.
+ * 5. take a sm variable, an keep a running sum while traversing backwards.
+ * 6. when sum >= index value; that index is h-index.
+ * TC: O(n) - 2 pass
+ * SC: O(n)
+ */
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+
+        int[] arr = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            int index = citations[i];
+            if (index > n)
+                arr[n]++;
+            else
+                arr[index]++;
+        }
+
+        // go backward from the right side and maintain a sum
+        int sum = 0;
+        for (int i = n; i > 0; i--) {
+            sum = sum + arr[i];
+            if (sum >= i)
+                return i;
         }
         // never found it
         return 0;
